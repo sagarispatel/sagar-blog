303bc75c89e24908d5c334c6c6bfd2aaa86bb760
MakerSquare Student <students@themakersquare.com>
2013-07-01 14:35:30 -0500

    Initial Commit/added vendor/bundle and DS_Store to gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SagarsBlog::Application.config.secret_token = '12ab17309488b560a8e77760a0402258094992617fa06d7beb74017aa9bcf26c5da67ce7855b8d0aee98f05cde9dd01d0dc54f9ce11fadae873aae7ad8778ea7'
